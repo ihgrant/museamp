eb68eb196d0dd0288bf00a2a2849501817e7e81b
Ian <ian.h.grant@gmail.com>
2017-04-23 09:30:24 -0700

    implements tap tests for reducer

---
+++ test/reducers.spec.js
@@ -0,0 +1,90 @@
+const test = require('tape');
+import reducer from '../src/reducers';
+
+test('reducers', function(t) {
+    let state = reducer(undefined, {});
+
+    t.deepEqual(
+        state,
+        {
+            chosenSongId: 0,
+            chosenPlaylistId: -1,
+            library: [],
+            paused: true,
+            playlists: []
+        },
+        'returns initial state'
+    );
+
+    state = reducer(state, { type: 'ADD_PLAYLIST', name: 'test' });
+
+    t.deepEqual(
+        state,
+        {
+            chosenSongId: 0,
+            chosenPlaylistId: -1,
+            library: [],
+            paused: true,
+            playlists: [{ name: 'test', songIds: [] }]
+        },
+        'adds a playlist'
+    );
+
+    state = reducer(state, { type: 'CHOOSE_PLAYLIST', id: 0 });
+
+    t.deepEqual(
+        state,
+        {
+            chosenSongId: 0,
+            chosenPlaylistId: 0,
+            library: [],
+            paused: true,
+            playlists: [{ name: 'test', songIds: [] }]
+        },
+        'chooses a playlist'
+    );
+
+    state = reducer(state, { type: 'PLAYLIST_ADD_SONG', id: 0 });
+
+    t.deepEqual(
+        state,
+        {
+            chosenSongId: 0,
+            chosenPlaylistId: 0,
+            library: [],
+            paused: true,
+            playlists: [{ name: 'test', songIds: [0] }]
+        },
+        'adds a song to a playlist'
+    );
+
+    state = reducer(state, { type: 'PLAYLIST_ADD_SONG', id: 1 });
+
+    t.deepEqual(
+        state,
+        {
+            chosenSongId: 0,
+            chosenPlaylistId: 0,
+            library: [],
+            paused: true,
+            playlists: [{ name: 'test', songIds: [0, 1] }]
+        },
+        'adds another song to a playlist'
+    );
+
+    state = reducer(state, { type: 'PLAYLIST_REMOVE_SONG', id: 0 });
+
+    t.deepEqual(
+        state,
+        {
+            chosenSongId: 0,
+            chosenPlaylistId: 0,
+            library: [],
+            paused: true,
+            playlists: [{ name: 'test', songIds: [1] }]
+        },
+        'removes a song from a playlist'
+    );
+
+    t.end();
+});
